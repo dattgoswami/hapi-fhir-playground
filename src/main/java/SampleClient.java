2b16cd950b3ec5b3645ef84a2fa2ea69ee1f4023
Datt Goswami <dattgoswami@gmail.com>
2023-06-15 22:07:49 -0600

    basic tasks completed

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -4,6 +4,10 @@ import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
 
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
 public class SampleClient {
 
     public static void main(String[] theArgs) {
@@ -21,7 +25,46 @@ public class SampleClient {
                 .returnBundle(Bundle.class)
                 .execute();
 
+        printPatientInformation(response);
+    }
+    private static void printPatientInformation(Bundle response) {
+        List<Bundle.BundleEntryComponent> entries = response.getEntry();
 
+        // Sort the response by first name
+        entries.sort(Comparator.comparing(entry -> getFirstName(entry)));
+
+        // Print Patient information
+        for (Bundle.BundleEntryComponent entry : entries) {
+            Patient patient = extractPatient(entry);
+            String firstName = getFirstName(patient);
+            String lastName = getLastName(patient);
+            Date birthDate = patient.getBirthDate();
+
+            System.out.println("First Name: " + firstName);
+            System.out.println("Last Name: " + lastName);
+            System.out.println("Birth Date: " + birthDate);
+        }
+    }
+
+    private static String getFirstName(Bundle.BundleEntryComponent entry) {
+        Patient patient = extractPatient(entry);
+        return getFirstName(patient);
+    }
+
+    private static String getFirstName(Patient patient) {
+        return patient.getNameFirstRep().getGivenAsSingleString();
+    }
+
+    private static String getLastName(Patient patient) {
+        return patient.getNameFirstRep().getFamily();
+    }
+
+    private static Patient extractPatient(Bundle.BundleEntryComponent entry) {
+        if (entry.hasResource() && entry.getResource() instanceof Patient) {
+            return (Patient) entry.getResource();
+        } else {
+            throw new IllegalArgumentException("Invalid entry or resource type");
+        }
     }
 
 }
